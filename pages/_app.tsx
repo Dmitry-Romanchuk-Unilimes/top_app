401fd568c1fd48d40c966eb51f281c5cee696a00
Dmitry-Romanchuk-Unilimes <dmitry.romanchuk@unilimes.coms>
2021-09-03 13:13:59 +0300

    watch video 36

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import { AppProps } from 'next/dist/shared/lib/router/router';
+import React from 'react';
+import Head from 'next/head';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return <>
+    <Head>
+      <title>My top app</title>
+      <meta name="description" content="My top app" />
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap" rel="stylesheet" />
+    </Head>
+    <Component {...pageProps} />
+  </>;
 }
 
 export default MyApp;
